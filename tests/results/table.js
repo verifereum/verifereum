c4d23e8d5d924115d6713d90780f4fcef6559351
Ramana Kumar <ramana@member.fsf.org>
2025-05-14 20:13:45 +0100

    Add hide input for timeouts

--- tests/results/table.js
+++ tests/results/table.js
@@ -1,18 +1,27 @@
-const label = document.createElement('label')
-const span = document.createElement('span')
-span.textContent = ' hide passed'
-const checkbox = document.createElement('input')
-checkbox.type = 'checkbox'
-checkbox.addEventListener('change', (e) => {
-  if (e.currentTarget.checked) {
-    document.querySelectorAll('tr:has(.pass)').forEach((tr) =>
-      tr.classList.add('hide'))
-  } else {
-    document.querySelectorAll('tr:has(.pass)').forEach((tr) =>
-      tr.classList.remove('hide'))
-  }
-})
-label.append(checkbox, span)
+const mk_label = (hidecls) => {
+  const label = document.createElement('label')
+  const span = document.createElement('span')
+  span.textContent = ` hide ${hidecls}`
+  const selector = `tr:has(.${hidecls})`
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  checkbox.addEventListener('change', (e) => {
+    if (e.currentTarget.checked) {
+      document.querySelectorAll(selector).forEach((tr) =>
+        tr.classList.add('hide'))
+    } else {
+      document.querySelectorAll(selector).forEach((tr) =>
+        tr.classList.remove('hide'))
+    }
+  })
+  label.append(checkbox, span)
+  return label
+}
+
+const div = document.createElement('div')
+div.append(mk_label('passed'),
+           mk_label('timeout'))
+
 document.addEventListener('DOMContentLoaded', () =>
   document.querySelector('body').insertBefore(
-    label, document.querySelector('table')))
+    div, document.querySelector('table')))
